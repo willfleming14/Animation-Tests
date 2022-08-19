4012d21c95927598f61ae11d13ee988a8bac98bf
William Fleming <sc20wrpf@leeds.ac.uk>
2022-08-19 15:51:37 +0100

    Uploaded website v2

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 var tl = gsap.timeline();
-tl.fromTo(".slider", 3, { y: "0%" }, { y: "-100%", ease: Power2.easeInOut });
-tl.fromTo(".loadImage", 1.5, { opacity: 1, scale: 0.6 }, { opacity: 0, scale: 0.5, ease: Power2.easeInOut }, "-=3");
-tl.fromTo(".name", 2, { y: "100%", opacity: 0 }, { y: "0%", opacity: 1, ease: Power2.easeInOut }, "-=1.75");
-tl.fromTo(".uni", 2, { y: "100%", opacity: 0 }, { y: "0%", opacity: 1, ease: Power2.easeInOut }, "-=1.75");
+tl.fromTo(".slider", 2, { y: "0%" }, { y: "-100%", ease: Power2.easeInOut });
+tl.fromTo(".loadImage", 1, { opacity: 1, scale: 0.6 }, { opacity: 0, scale: 0.5, ease: Power2.easeInOut }, "-=2");
+tl.fromTo(".name", 2, { y: "100%", opacity: 0 }, { y: "0%", opacity: 1, ease: Power2.easeInOut }, "-=1.25");
+tl.fromTo(".uni, .icon", 2, { y: "100%", opacity: 0 }, { y: "0%", opacity: 1, ease: Power2.easeInOut }, "-=1.75");
 tl.fromTo("hr", 2, { width: "0%" }, { width: "90%", ease: Power2.easeInOut }, "-=1");
+
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.from('.secondPage', {
@@ -12,7 +13,7 @@ gsap.from('.secondPage', {
         start: "top 40%",
         end: "center bottom",
         scrub: 1,
-        markers: true,
+        markers: false,
     }
 });
 
@@ -39,6 +40,16 @@ gsap.from('.fourthPage', {
 gsap.from('.fifthPage', {
     x: "-150%", opacity: 0, duration: 5, scrollTrigger: {
         trigger: '.fifthPage',
+        start: "top 40%",
+        end: "center bottom",
+        scrub: 1,
+        markers: false,
+    }
+});
+
+gsap.from('.sixthPage', {
+    x: "-150%", opacity: 0, duration: 5, scrollTrigger: {
+        trigger: '.sixthPage',
         start: "top 40%",
         end: "center bottom",
         scrub: 1,
